6e107602b855cfb2e98431e18282147a4e9d6ce4
fred1653 <fred1653@qq.com>
2021-03-05 12:34:24 +0800

    add bugs

--- serv/api/routes/images.py
+++ serv/api/routes/images.py
@@ -23,8 +23,6 @@ async def get_image(image_hash: str, p: int = 1, w: int = 1, h: int = 1, r: int 
         img = await Image_Pydantic.from_queryset_single(Images.get(hash=image_hash))
     except Exception as e:
         raise HTTPException(status_code=404, detail="image not found")
-    # todo: add image processing
-    # image = await get_file(img.storename)
     image = processing(img.storename, p, w, h, r, q, f)
     image.seek(0)
     return StreamingResponse(image, media_type=img.content_type)
