4494f4e8b3370c2c73fb53d0158593c48777e03f
fred1653 <fred1653@qq.com>
2021-04-03 14:55:45 +0800

    update config

--- serv/core/config.py
+++ serv/core/config.py
@@ -1,5 +1,5 @@
 import logging
-import sys
+import secrets
 from typing import List
 
 from loguru import logger
@@ -13,25 +13,31 @@ API_PREFIX = "/api"
 JWT_TOKEN_PREFIX = "Token"
 VERSION = "0.0.1"
 
-config = Config()
+# config = Config()
 
-DEBUG: bool = config("DEBUG", cast=bool, default=True)
+# DEBUG: bool = config("DEBUG", cast=bool, default=True)
+DEBUG: bool = True
 
 # DATABASE
 # DATABASE: str = config("DB_CONNECTION", cast=str, default="sqlite://./test.db")
-DATABASE: str = config("DB_CONNECTION", cast=str, default="postgres://postgres:postgres@postgres/postgres")
+# DATABASE: str = config("DB_CONNECTION", cast=str, default="postgres://postgres:postgres@postgres/postgres")
+DATABASE: str = "postgres://postgres:postgres@postgres/postgres"
 
-SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret, default="Secret")
+# SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret, default="Secret")
+SECRET_KEY: Secret = secrets.token_urlsafe(32)
 
-PROJECT_NAME: str = config("PROJECT_NAME", default="FastAPI example application")
-ALLOWED_HOSTS: List[str] = config(
-    "ALLOWED_HOSTS",
-    cast=CommaSeparatedStrings,
-    default="*",
-)
+# PROJECT_NAME: str = config("PROJECT_NAME", default="FastAPI example application")
+PROJECT_NAME: str = "Image Serv"
+# ALLOWED_HOSTS: List[str] = config(
+#     "ALLOWED_HOSTS",
+#     cast=CommaSeparatedStrings,
+#     default="",
+# )
+ALLOWED_HOSTS: List[str] = [""]
 
 # store path
-STORE_PATH: str = config("STORE_PATH", cast=str, default="/data/images")
+# STORE_PATH: str = config("STORE_PATH", cast=str, default="/data/images")
+STORE_PATH: str = "/app/images"
 
 # logging configuration
 
