e079d235111edd79fe2b630d956087c7a1166dfd
fred1653 <fred1653@qq.com>
2021-04-03 15:00:23 +0800

    update docker settings

--- Dockerfile
+++ Dockerfile
@@ -2,21 +2,21 @@ FROM python:3.8.1-slim
 
 ENV PYTHONUNBUFFERED 1
 
-EXPOSE 8000
+EXPOSE 80
+
 WORKDIR /app
 
 RUN apt-get update && \
     apt-get install -y --no-install-recommends netcat python3-dev python3-pip && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-COPY poetry.lock pyproject.toml ./
-RUN pip3 install poetry==1.1 && \
-    poetry config virtualenvs.in-project true && \
-    poetry install --no-dev
-
 COPY ./serv /app/serv
 
 COPY .env.example /app/.env
 
-CMD poetry run alembic upgrade head && \
-    poetry run uvicorn --host=0.0.0.0 serv.main:app
+COPY requirements.txt /app/requirements.txt
+
+RUN pip3 install -r requirements.txt && \
+    pip3 instal -e .
+
+CMD ["uvicorn", "serv.main:app", "--host", "0.0.0.0", "--port", "80"]
--- serv/core/config.py
+++ serv/core/config.py
@@ -13,31 +13,31 @@ API_PREFIX = "/api"
 JWT_TOKEN_PREFIX = "Token"
 VERSION = "0.0.1"
 
-# config = Config()
+config = Config(".env")
 
-# DEBUG: bool = config("DEBUG", cast=bool, default=True)
-DEBUG: bool = True
+DEBUG: bool = config("DEBUG", cast=bool, default=True)
+# DEBUG: bool = True
 
 # DATABASE
-# DATABASE: str = config("DB_CONNECTION", cast=str, default="sqlite://./test.db")
+DATABASE: str = config("DB_CONNECTION", cast=str, default="sqlite://./test.db")
 # DATABASE: str = config("DB_CONNECTION", cast=str, default="postgres://postgres:postgres@postgres/postgres")
-DATABASE: str = "postgres://postgres:postgres@postgres/postgres"
+# DATABASE: str = "postgres://postgres:postgres@postgres/postgres"
 
-# SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret, default="Secret")
-SECRET_KEY: Secret = secrets.token_urlsafe(32)
+SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret, default=secrets.token_urlsafe(32))
+# SECRET_KEY: Secret = secrets.token_urlsafe(32)
 
-# PROJECT_NAME: str = config("PROJECT_NAME", default="FastAPI example application")
-PROJECT_NAME: str = "Image Serv"
-# ALLOWED_HOSTS: List[str] = config(
-#     "ALLOWED_HOSTS",
-#     cast=CommaSeparatedStrings,
-#     default="",
-# )
-ALLOWED_HOSTS: List[str] = [""]
+PROJECT_NAME: str = config("PROJECT_NAME", default="Image Serv")
+# PROJECT_NAME: str = "Image Serv"
+ALLOWED_HOSTS: List[str] = config(
+    "ALLOWED_HOSTS",
+    cast=CommaSeparatedStrings,
+    default=[""],
+)
+# ALLOWED_HOSTS: List[str] = [""]
 
 # store path
-# STORE_PATH: str = config("STORE_PATH", cast=str, default="/data/images")
-STORE_PATH: str = "/app/images"
+STORE_PATH: str = config("STORE_PATH", cast=str, default="/data/images")
+
 
 # logging configuration
 
