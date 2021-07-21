9c044ba1a17f3361131606c31bae3e59ddc2bd56
JiamingHu <me@hoooo.org>
2021-07-21 22:48:45 +0800

    update frontends

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Search />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Search from './components/Search.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Search
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="md:container md:mx-auto">
+    <h1>Search</h1>
+    <input type="file" />
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Search',
+  props: {
+
+  },
+  setup: () => {
+
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
